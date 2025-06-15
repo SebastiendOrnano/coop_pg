a855adac0ce4e9af9720130fe5d66eb0c92315f7
SdO <sdornano@gmail.com>
2025-06-15 11:57:26 +0200

    Add files via upload

---
+++ a_rss/rss_main_display_4.sql
@@ -0,0 +1,23 @@
+SELECT 'dynamic' AS component, sqlpage.run_sql('a_shells/shell_4.sql') AS properties;
+SELECT 
+    'title'        AS component,
+    'RSS'         AS title,
+    'Liste des liens à copier pour s''abonner à un flux RSS' AS contents;
+
+SELECT 
+    'divider' AS component;
+
+SELECT 
+    'table'               AS component, 
+    'Étapes du processus' AS title, 
+    TRUE                  AS sort, 
+    'View'                AS markdown, 
+    FALSE                 AS search;
+
+SELECT
+    rss_title              AS Title,
+    rss_feed               AS link
+FROM rss
+WHERE rss_category='supervisor'
+ORDER BY created_at ASC;
+
---
+++ a_rss/rss_main_display_5.sql
@@ -0,0 +1,51 @@
+SELECT 'dynamic' AS component, sqlpage.run_sql('a_shells/shell_5.sql') AS properties;
+
+SELECT 
+    'breadcrumb' AS component;
+SELECT 
+    'Home' AS title,
+    '/'    AS link;
+SELECT 
+    'Gestion Admin'         AS title,
+    '/a_panels/panel_admin_5.sql' AS link;
+
+
+
+SELECT 
+    'datagrid'               AS component,
+    'process_step_display'   AS id;
+SELECT 
+    '/a_rss/rss_main_form_5.sql'    AS link,
+    'Créer un  lien RSS'                                       AS description,
+    'rss'                                                      AS icon,
+    'green'                                                     AS color;
+SELECT 
+    '/a_rss/rss_archive_display_5.sql'    AS link,
+    'liens RSS archivés'                                       AS description,
+    'archive'                                                  AS icon,
+    'blue'                                                    AS color;
+
+SELECT 
+    'title'        AS component,
+    'RSS'         AS title,
+    'Liste des liens à copier pour s''abonner à un flux RSS' AS contents;
+
+SELECT 
+    'divider' AS component;
+
+SELECT 
+    'table'               AS component, 
+    'Étapes du processus' AS title, 
+    TRUE                  AS sort, 
+    'View'                AS markdown, 
+    FALSE                 AS search;
+
+SELECT
+    rss_id                 AS Id,
+    rss_title              AS Title,
+    rss_feed               AS link,
+    rss_category           AS Catégorie,
+   '[Edit](/a_rss/rss_main_edit_5.sql?rss_id='||rss_id|| ') &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; [Delete](/a_rss/rss_main_delete_0.sql?rss_id='||rss_id||')'    AS View
+FROM rss WHERE rss_status ='active'
+ORDER BY created_at ASC;
+
