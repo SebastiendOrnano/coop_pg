853c271358d1e5d8a0779c6c2894db81df772658
SdO <sdornano@gmail.com>
2025-06-15 11:58:05 +0200

    Add files via upload

---
+++ a_scripts/printdiv.js
@@ -0,0 +1,9 @@
+function printdiv(){
+    var print_div = document.getElementById("sqlpage_main_wrapper");
+    var print_area = window.open();
+    print_area.document.write(print_div.innerHTML);
+    print_area.document.close();
+    print_area.focus();
+    print_area.print();
+    print_area.close();
+    }
