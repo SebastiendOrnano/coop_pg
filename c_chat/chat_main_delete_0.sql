2095a03663758bc4cecbe09150187f38e0fbc0a8
SdO <sdornano@gmail.com>
2025-12-30 15:11:28 +0100

    Delete c_chat directory

--- c_chat/chat_main_delete_0.sql
+++
@@ -1,6 +0,0 @@
-DELETE FROM forum_messages WHERE topic_id = $topic_id;
-
-DELETE FROM forum_topics WHERE topic_id = $topic_id
-RETURNING
-   'redirect' AS component,
-   '/c_chat/chat_main_display_5.sql' AS link;
