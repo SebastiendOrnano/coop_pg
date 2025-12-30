293669f66f8e9ba022cd8ef719a17075147b24aa
SdO <sdornano@gmail.com>
2025-12-30 15:22:03 +0100

    Delete e_project directory

--- e_project/project_main_create_alert1_4.sql
+++
@@ -1,8 +0,0 @@
-SELECT 'dynamic' AS component, sqlpage.run_sql('a_shells/shell_4.sql') AS properties;
-
-SELECT 'alert' AS component,
-    'Attention' AS title,
-    'Il existe déjà un projet avec ce nom. Choississez un autre nom.  
-[Reprenez votre saisie](/e_project/project_main_form_4.sql)' AS description_md,
-    'alert-circle' AS icon,
-    'red' AS color;
