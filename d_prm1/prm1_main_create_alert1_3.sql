943e499e66128b13504e40da0d8cf772b0b18d2f
SdO <sdornano@gmail.com>
2025-12-30 15:14:58 +0100

    Delete d_prm1 directory

--- d_prm1/prm1_main_create_alert1_3.sql
+++
@@ -1,9 +0,0 @@
-SELECT 'dynamic' AS component, sqlpage.run_sql('a_shells/shell_3.sql') AS properties;
-
-SELECT 'alert' as component,
-    'Attention' as title,
-    'Le couple prénom /nom de famille saisie est déjà utilisé.  
-    Vérifier si cette personne n''a pas déjà été créé. S''il s''agit d''un homonyme, utilisez 2 prénoms dans le champ prénom
-[Reprenez votre saisie](/d_prm1/prm1_main_form_3.sql)' AS description_md,
-    'alert-circle' as icon,
-    'red' as color;
--- d_prm1/prm1_main_create_alert1_4.sql
+++
@@ -1,9 +0,0 @@
-SELECT 'dynamic' AS component, sqlpage.run_sql('a_shells/shell_4.sql') AS properties;
-
-SELECT 'alert' as component,
-    'Attention' as title,
-    'Le couple prénom /nom de famille saisie est déjà utilisé.  
-    Vérifier si cette personne n''a pas déjà été créé. S''il s''agit d''un homonyme, utilisez 2 prénoms dans le champ prénom
-[Reprenez votre saisie](/d_prm1/prm1_main_form_4.sql)' AS description_md,
-    'alert-circle' as icon,
-    'red' as color;
