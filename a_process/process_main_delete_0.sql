8c52966cc424f9dbbda021d2a1b1e330587842f4
SdO <sdornano@gmail.com>
2025-12-30 14:52:08 +0100

    Delete a_process directory

--- a_process/process_main_delete_0.sql
+++
@@ -1,9 +0,0 @@
-DELETE FROM process_steps
-WHERE process_id = $process_id::INTEGER 
-
-
-DELETE FROM process
-WHERE process_id = $process_id::INTEGER 
-RETURNING
-   'redirect' AS component,
-   '/a_process/process_main_display_5.sql' AS link;
