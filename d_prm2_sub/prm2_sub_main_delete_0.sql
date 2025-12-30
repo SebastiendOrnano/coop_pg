ce52b641873349e83014d7ae133d63b6e5cfdc96
SdO <sdornano@gmail.com>
2025-12-30 15:17:17 +0100

    Delete d_prm2_sub directory

--- d_prm2_sub/prm2_sub_main_delete_0.sql
+++
@@ -1,6 +0,0 @@
-SET prm2_id = SELECT prm2_id FROM prm2_sub WHERE prm2_sub_id  = $prm2_sub_id::INTEGER;
-
-DELETE FROM prm2_sub WHERE prm2_sub_id  = $prm2_sub_id::INTEGER
-RETURNING
-'redirect' AS component,
-'/d_prm2_sub/prm2_sub_main_display_4.sql?prm2_id='||$prm2_id AS link;
