b08e9733f3aa6f095ed1b9454dd0ae9b8de683d3
SdO <sdornano@gmail.com>
2025-12-30 14:47:01 +0100

    Delete a_choices directory

--- a_choices/choice_category_delete_0.sql
+++
@@ -1,6 +0,0 @@
-DELETE FROM choices_categories
-WHERE choice_category_id  = $choice_category_id::INTEGER 
-
-RETURNING
-'redirect' AS component,
-'/a_choices/choice_category_display_5.sql' AS link;
--- a_choices/choice_main_delete_0.sql
+++
@@ -1,6 +0,0 @@
-DELETE FROM choices_items
-WHERE choice_id  = $choice_id::INTEGER 
-
-RETURNING
-'redirect' AS component,
-'/a_choices/choice_main_display_5.sql' AS link;
