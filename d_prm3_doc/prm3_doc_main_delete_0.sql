9369dadffdf7d39a2d9b9317b9ef0a645c045228
SdO <sdornano@gmail.com>
2025-12-30 15:20:29 +0100

    Delete d_prm3_doc directory

--- d_prm3_doc/prm3_doc_main_delete_0.sql
+++
@@ -1,8 +0,0 @@
-SET prm3_doc_url= (SELECT prm3_doc_url  FROM prm3_docs WHERE prm3_doc_id = $prm3_doc_id::INTEGER );
-SET prm3_id = (SELECT prm3_id  FROM prm3_docs WHERE prm3_doc_id = $prm3_doc_id::INTEGER );
-
-DELETE FROM prm3_docs
-WHERE prm3_doc_id = $prm3_doc_id::INTEGER
-RETURNING
-   'redirect' AS component,
-   '/d_prm3_doc/prm3_doc_main_display_4.sql?prm3_id='||$prm3_id as link;
--- d_prm3_doc/prm3_doc_select_delete_0.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO files_to_erase (file_url)
-SELECT prm3_doc_url FROM prm3_docs WHERE  prm3_doc_selected = true;
-
-DELETE FROM prm3_docs
-WHERE prm3_doc_selected = true AND prm3_id=$prm3_id::INTEGER;
-
-RETURNING 
-'redirect' AS component, 
-'/d_prm3_doc/prm3_doc_select_no_0.sql?prm3_id='||$prm3_id AS link;
