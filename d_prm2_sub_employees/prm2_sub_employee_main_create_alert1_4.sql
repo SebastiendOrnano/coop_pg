f9e25a8fc7150702a5d10f6501ac427026c4a74b
SdO <sdornano@gmail.com>
2025-12-30 15:18:51 +0100

    Delete d_prm2_sub_employees directory

--- d_prm2_sub_employees/prm2_sub_employee_main_create_alert1_4.sql
+++
@@ -1,9 +0,0 @@
-SELECT 'dynamic' AS component, sqlpage.run_sql('a_shells/shell_4.sql') AS properties;
-
-SELECT 'alert' as component,
-    'Attention' as title,
-    'Le couple prénom /nom de famille saisie est déjà utilisé.  
-    Vérifier si la personne n''a pas déjà été créée. S''il s''agit d''un homonyme, utilisez 2 prénoms dans le champ prénom
-[Reprenez votre saisie](/d_prm2_sub_employees/prm2_sub_employee_main_form2_4.sql?prm2_sub_id='||$prm2_sub_id||')' AS description_md,
-    'alert-circle' as icon,
-    'red' as color;
--- d_prm2_sub_employees/prm2_sub_employee_main_create_alert2_4.sql
+++
@@ -1,9 +0,0 @@
-SELECT 'dynamic' AS component, sqlpage.run_sql('a_shells/shell_1.sql') AS properties;
-
-
-SELECT 'alert' as component,
-    'Attention' as title,
-    'L''email saisie est déjà utilisé. Vérifier si cet adhérent n''a pas déjà été créé. Si 2 adhérents partagent le même email, il faut leur demander d''utiliser des emails différents. *(NB : il est parfois possible de créer plusieurs alias sur un même compte mail)
-[Reprenez votre saisie](/d_prm2_sub_employees/prm2_sub_employee_main_form2_4.sql?prm2_sub_id='||$prm2_sub_id||')' AS description_md,
-    'alert-circle' as icon,
-    'red' as color;
