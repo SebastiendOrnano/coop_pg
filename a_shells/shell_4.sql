98a51293aa26915415c89ff27e108633c2276613
SdO <sdornano@gmail.com>
2025-12-30 14:56:19 +0100

    Add files via upload

---
+++ a_shells/shell_4.sql
@@ -0,0 +1,11 @@
+SET user_role = (
+    SELECT user_role FROM users
+    INNER JOIN sessions ON users.user_id = sessions.user_id
+    WHERE sessions.session_token = sqlpage.cookie('session_token')
+); 
+
+SELECT 'redirect' AS component,        
+'/a_sessions/session_create_alert4.sql' AS link 
+WHERE NOT ($user_role = 'supervisor') OR sqlpage.cookie('session_token') IS NULL;
+
+SELECT 'dynamic' AS component, sqlpage.run_sql('a_shells/shell_core.sql') AS properties;
