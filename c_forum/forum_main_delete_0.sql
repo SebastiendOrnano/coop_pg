159cf6d7a82655f354930878049a221d7bd3ae0d
SdO <sdornano@gmail.com>
2025-12-30 15:12:59 +0100

    Delete c_forum directory

--- c_forum/forum_main_delete_0.sql
+++
@@ -1,6 +0,0 @@
-DELETE FROM forum_messages WHERE topic_id = $topic_id::INTEGER ;
-
-DELETE FROM forum_topics WHERE topic_id = $topic_id::INTEGER 
-RETURNING
-   'redirect' AS component,
-   '/c_forum/forum_main_display_5.sql' AS link;
