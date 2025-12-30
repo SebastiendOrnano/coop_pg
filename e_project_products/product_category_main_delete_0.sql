ab037a2d86bb24c247ef7f505c495f027bde0db1
SdO <sdornano@gmail.com>
2025-12-30 15:27:38 +0100

    Delete e_project_products directory

--- e_project_products/product_category_main_delete_0.sql
+++
@@ -1,6 +0,0 @@
-DELETE FROM project_products_categories
-WHERE product_category_id  = $product_category_id
-
-RETURNING
-'redirect' AS component,
-'/e_project_products/product_category_main_display_4.sql' AS link;
