a35c0941de21b4304babc348f5b3a331e1420fdc
SdO <sdornano@gmail.com>
2025-12-30 15:34:22 +0100

    Delete e_project_worksequence directory

--- e_project_worksequence/worksequence_main_create_alert1_1.sql
+++
@@ -1,7 +0,0 @@
-SELECT 'dynamic' AS component, sqlpage.run_sql('a_shells/shell_1.sql') AS properties;
-
-SELECT 'alert' AS component,
-    'Attention' AS title,
-    'heure  de fin < heure de début ! [Reprenez votre saisie](/e_project_worksequence/worksequence_main_form'||$suffix_role||'?workpackage_id='||$workpackage_id||')' AS description_md,
-    'alert-circle' AS icon,
-    'red' AS color;
