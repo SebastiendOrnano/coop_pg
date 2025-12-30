5605508c7d990cb3f6f1f121b99881fa8b273865
SdO <sdornano@gmail.com>
2025-12-30 14:58:22 +0100

    Delete b_events directory

--- b_events/event_main_delete_0.sql
+++
@@ -1,7 +0,0 @@
-
-DELETE FROM events
-WHERE event_id  = $event_id::INTEGER
-
-RETURNING
-'redirect' AS component,
-'/b_events/event_main_display_4.sql' AS link;
--- b_events/event_participants_csv_email_0.sql
+++
@@ -1,5 +0,0 @@
-SELECT 'csv' 
-as component, 
-'event_participants_full_list.csv' AS filename;
-
-SELECT participant_first_name, participant_last_name, participant_contact_email, participant_contact_phone, participant_function FROM event_participants WHERE event_id=$event_id::INTEGER;
