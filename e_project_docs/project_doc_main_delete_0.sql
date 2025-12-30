d1ab1c3471cbe4b81396f69a0208718d9235d51a
SdO <sdornano@gmail.com>
2025-12-30 15:25:00 +0100

    Delete e_project_docs directory

--- e_project_docs/project_doc_main_delete_0.sql
+++
@@ -1,7 +0,0 @@
-SET project_id = SELECT project_id FROM project_docs WHERE project_doc_id = $project_doc_id::INTEGER;
-
-DELETE FROM project_docs
-WHERE project_doc_id = $project_doc_id::INTEGER
-RETURNING
-   'redirect' AS component,
-   '/e_project_docs/project_doc_main_display_4.sql?project_id='||$project_id AS link;
