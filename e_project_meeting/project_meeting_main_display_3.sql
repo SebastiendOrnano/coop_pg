d0170db52715c526ef819bf1b5c4cfb6ba71f215
SdO <sdornano@gmail.com>
2026-01-02 19:12:51 +0100

    Update SQL queries for project meeting display

--- e_project_meeting/project_meeting_main_display_3.sql
+++ e_project_meeting/project_meeting_main_display_3.sql
@@ -42,15 +42,14 @@ SELECT
 SELECT 'table' AS component, 
     'Liste des événements à venir'   AS title, 
     'Alert'          AS markdown,
-    JSON('{"name":"View","tooltip":"Voir le détail de la réunion","link":"/e_project_meeting/project_meeting_main_view_4.sql?project_meeting_id={id}","icon":"user-circle"}') as custom_actions,
-    JSON('{"name":"Participantsl","Liste des participants":"Voir le profil","link":"/e_project_meeting/project_meeting_participants_display_4.sql?project_meeting_id={id}","icon":"users"}') as custom_actions,
+    JSON('{"name":"View","tooltip":"Voir le détail de la réunion","link":"/e_project_meeting/project_meeting_main_view_3.sql?project_meeting_id={id}","icon":"user-circle"}') as custom_actions,
+    JSON('{"name":"Participantsl","Liste des participants":"Voir le profil","link":"/e_project_meeting/project_meeting_participants_display_3.sql?project_meeting_id={id}","icon":"users"}') as custom_actions,
     TRUE             AS sort, 
     TRUE             AS small,
     TRUE             AS search;
 
 SELECT
     CASE WHEN (SELECT e.project_meeting_alert IS NULL OR e.project_meeting_alert = 'alert_end') THEN '' ELSE '![warning](/x_icons/warning32px.png)' END  AS  Alert,
-   
     e.project_meeting_title             AS Nom,
     e.project_meeting_id             AS _sqlpage_id,
     e.project_meeting_status           AS statut,
@@ -60,16 +59,18 @@ SELECT
     e.project_meeting_date             AS Date,
     e.project_meeting_hour_start       AS heure,
     p.project_meeting_place_name       AS Lieu,
-   SELECT user_id
-    FROM project_meeting_participants
-    WHERE project_meeting_participant_role = 'coordinator'
-    AND project_meeting_id = e.project_meeting_id::integer
-    LIMIT 1
-    ) = $user_id::integer
-    OR $user_role = 'supervisor'
-    THEN JSON('{"name":"Edit","tooltip":"Voir le détail de la réunion","link":"/e_project_meeting/project_meeting_main_edit_4.sql?project_meeting_id='||e.project_meeting_id||'","icon":"edit"}')
-    ELSE  JSON('{"name":"Edit"}')
-    END   AS _sqlpage_actions
+    CASE
+    WHEN (
+SELECT user_id
+FROM project_meeting_participants
+WHERE project_meeting_participant_role = 'coordinator'
+AND project_meeting_id = e.project_meeting_id::integer
+LIMIT 1
+) = $user_id::integer
+OR $user_role = 'supervisor'
+THEN JSON('{"name":"Edit","tooltip":"Voir le détail de la réunion","link":"/e_project_meeting/project_meeting_main_edit_3.sql?project_meeting_id='||e.project_meeting_id||'","icon":"Edit"}')
+ELSE  JSON('{"name":"Edit"}')
+END   AS _sqlpage_actions
 FROM project_meeting AS e
 LEFT JOIN (SELECT project_meeting_place_name, project_meeting_place_id FROM project_meeting_places ) AS p 
 ON p.project_meeting_place_id = e.project_meeting_place_id
