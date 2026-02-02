9940f9f8ecf4878fcdd2bb8dece941a5da0018a6
SdO <sdornano@gmail.com>
2026-02-02 16:52:51 +0100

    Fix SQL syntax for alert message in session_create_alert1

--- a_sessions/session_create_alert1.sql
+++ a_sessions/session_create_alert1.sql
@@ -2,7 +2,7 @@ SELECT 'dynamic' AS component, sqlpage.run_sql('a_shells/shell.sql') AS properti
 
 SELECT 'alert' AS component,
     'Attention' AS title,
-    'Il n''y a pas de compte associé à cet email. Soit vous avez mal saisi votre email, soit vous n'avez pas de compte.
+    'Il n''y a pas de compte associé à cet email. Soit vous avez mal saisi votre email, soit vous n''avez pas de compte.
 
 [Reprenez votre saisie](/a_sessions/session_main_form.sql) ou [Créez un compte](/a_users/user_main_form.sql)  ou [contactez l''administrateur](/b_faq/faq_main_form.sql)' AS description_md,
     'alert-circle' AS icon,
