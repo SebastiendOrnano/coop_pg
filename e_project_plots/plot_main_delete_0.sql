e77eeaf0753c3d412efbf3e6a84c02766660ed23
SdO <sdornano@gmail.com>
2026-02-03 14:54:53 +0100

    Delete e_project_plots directory

--- e_project_plots/plot_main_delete_0.sql
+++
@@ -1,6 +0,0 @@
-set project_id = SELECT project_id FROM project_plots WHERE plot_id = $plot_id::INTEGER;
-
-DELETE FROM project_plots WHERE plot_id  = $plot_id::INTEGER 
-RETURNING
-'redirect' AS component,
-'/e_project_plots/plot_main_display_4.sql?project_id='||project_id AS link;
--- e_project_plots/plot_select_delete_0.sql
+++
@@ -1,8 +0,0 @@
-SET project_id = SELECT project_id FROM project_plots WHERE plot_id = $plot_id::INTEGER;
-
-DELETE FROM project_plots
-WHERE plot_selected = true
-
-RETURNING 
-'redirect' AS component, 
-'/e_project_plots/plot_select_display_4.sql?plot_id='||$plot_id||'&project_id='||$project_id AS link;
