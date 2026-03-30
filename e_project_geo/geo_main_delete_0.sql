dff891540076ab1e43be49be84d8df85998ca79d
SdO <sdornano@gmail.com>
2026-03-30 13:28:20 +0200

    Delete e_project_geo directory

--- e_project_geo/geo_main_delete_0.sql
+++
@@ -1,6 +0,0 @@
-set project_id = SELECT project_id FROM project_geo WHERE geo_id=$geo_id::INTEGER;
-
-DELETE FROM project_geo WHERE geo_id  = $geo_id::INTEGER 
-RETURNING
-'redirect' AS component,
-'/e_project_geo/geo_main_display_4.sql?project_id='||project_id AS link;
